6356e1ed2c4d355107d358a7ab931c9151539e1e
devian <iyanski@gmail.com>
2011-03-01 12:12:19 +0800

    modified migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   
   acts_as_extjs_store(
-    :include => [
-      "id",
-      "email",
-      "contact_number",
-      "company_name",
-      "is_active",
-      "color"]
-    ,
+    :include => ["id", "email", "contact_number", "company_name", "is_active", "color"],
     :filters => {
       :is_active => lambda{|bool| bool == 1 ? "Yes" : "No"},
       :created_at => lambda{|at| at.to_s(:timezone) },
