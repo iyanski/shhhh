a70d8d8b84b59b93a981b5f956d1692ecbe35fbe
devian <iyanski@gmail.com>
2011-03-01 12:11:38 +0800

    modified migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
       "company_name",
       "is_active",
       "color"]
-      ],
+    ,
     :filters => {
       :is_active => lambda{|bool| bool == 1 ? "Yes" : "No"},
       :created_at => lambda{|at| at.to_s(:timezone) },
