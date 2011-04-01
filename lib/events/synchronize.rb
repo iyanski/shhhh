1ac77725f035d0f54fdcbcba2cbf42488b7351e0
devian <iyanski@gmail.com>
2011-04-01 21:30:46 +0800

    ..

--- lib/events/synchronize.rb
+++ lib/events/synchronize.rb
@@ -11,7 +11,6 @@ class Synchronize
         raw = `exiftool -all:Description -struct -j #{filename}`
         iptc = ActiveSupport::JSON::decode(raw)
         Photo.create(:post_id => post.id, :album_id => post.albums.first.id, :file_name => filename, :file_type => File.extname(iptc[0]['SourceFile']), :caption => "#{iptc[0]['Description']}")
-        Rails.logger.info(photo)
       end
       operation.status = 1
       operation.save
