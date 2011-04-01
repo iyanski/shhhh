2ddc957c059a74cc8370b7c379c7c2cd1c17e296
devian <iyanski@gmail.com>
2011-04-01 21:08:47 +0800

    ...

--- lib/events/synchronize.rb
+++ lib/events/synchronize.rb
@@ -10,8 +10,7 @@ class Synchronize
       dir.each do |filename|
         raw = `exiftool -all:Description -struct -j #{filename}`
         iptc = ActiveSupport::JSON::decode(raw)
-        photo = Photo.new({:post_id => post.id, :album_id => post.albums.first.id, :file_name => filename, :file_type => File.extname(iptc[0]['SourceFile']), :caption => "#{iptc[0]['Description']}"})
-        photo.save
+        Photo.create(:post_id => post.id, :album_id => post.albums.first.id, :file_name => filename, :file_type => File.extname(iptc[0]['SourceFile']), :caption => "#{iptc[0]['Description']}")
         Rails.logger.info(photo)
       end
       operation.status = 1
